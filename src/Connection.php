67011bbd1e9dd57f8ad7d08456c0d391def99664
klinson <klinson@163.com>
2020-01-09 18:41:25 +0800

    must be compatible with Illuminate\Database\Connection::rollBack($toLevel = NULL)

--- src/Connection.php
+++ src/Connection.php
@@ -348,7 +348,7 @@ class Connection extends BaseConnection
      * rollback transaction in this session and close this session
      * @author klinson <klinson@163.com>
      */
-    public function rollBack()
+    public function rollBack($toLevel = null)
     {
         if ($session = $this->getSession()) {
             $session->abortTransaction();
