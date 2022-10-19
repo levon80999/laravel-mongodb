18b7a521b1ed6728040a2f0334cc992cfd3c9a77
levon80999 <levonb@ucraft.com>
2022-10-19 18:32:44 +0400

    check connection

--- tests/config/database.php
+++ tests/config/database.php
@@ -13,10 +13,6 @@ return [
             'driver' => 'mongodb',
             'host' => $mongoHost,
             'database' => env('MONGO_DATABASE', 'unittest'),
-            'options'   => [
-                'replicaSet'                => 'rs',
-                'serverSelectionTryOnce'    => false,
-            ],
         ],
 
         'dsn_mongodb' => [
